ccb4fc888709f2ff7ba24571ed4033c95d277b17
Gustavo <gstvrrds@gmail.com>
2025-03-08 16:27:09 -0300

    feat: adding linear gradient to background

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import StartGame from "./screens/StartGame";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       <StartGame />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: "#ddb52f",
     flex: 1,
   },
 });
