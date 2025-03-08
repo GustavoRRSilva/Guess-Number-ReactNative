23145a54c7dbdcdf2301f0dea57d6a699c08eaae
Gustavo <gstvrrds@gmail.com>
2025-03-08 08:02:24 -0300

    feat:styling input container for android and  ios

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,13 +1,31 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGame() {
   return (
-    <View>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.bg}>
+      <View style={styles.inputContainer}>
+        <TextInput />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  bg: { backgroundColor: "#fff", flex: 1 },
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 6,
+  },
+});
 export default StartGame;
