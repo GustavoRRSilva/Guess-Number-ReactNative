d75aa96ba4b76bc6637c93520ece9385c88a50a0
Gustavo <gstvrrds@gmail.com>
2025-03-09 09:54:17 -0300

    feat:adding SafeAreaView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGame from "./screens/StartGame";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
@@ -25,7 +25,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,34 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 const GameScreen = () => {
+  const [guessedValue, setGuessedValue] = useState(0);
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Opponent's Guess</Text>
+      <View>
+        <Text>{guessedValue}</Text>
+        <Text>Higher or lower?</Text>
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+            <PrimaryButton>+</PrimaryButton>
+          </View>
+          <View style={styles.button}>
+            <PrimaryButton>-</PrimaryButton>
+          </View>
+        </View>
+      </View>
+      <View>Log rounds</View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default GameScreen;
